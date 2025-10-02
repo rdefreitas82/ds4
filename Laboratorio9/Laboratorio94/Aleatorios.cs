8e717c76300ee42f23e97501212d1ca1b5980e72
Ricardo De Freitas <rdefreitas@gmail.com>
2025-10-01 21:10:49 -0500

    Actualización a clase Aletorios para que al generar un arreglo verifique que todos los valores son diferentes.

--- Laboratorio9/Laboratorio94/Aleatorios.cs
+++ Laboratorio9/Laboratorio94/Aleatorios.cs
@@ -5,15 +5,33 @@
         private static Random random = new Random();
         public static int generar(int min, int max)
         {
-            return random.Next(min, max);
+            return random.Next(min, max + 1);
         }
 
         public static int[] arreglo(int n, int min, int max)
         {
             int[] arreglo = new int[n];
+
+            if (n > ((max + 1) - min))
+                throw new ArgumentException("El tamaño del arreglo es mayor que el rango de valores posibles, por lo que los valores no pueden ser unicos.");
+
+
             for (int i = 0; i < n; i++)
             {
                 arreglo[i] = generar(min, max);
+                for (int j = 0; j < i; j++)
+                {
+                    if (arreglo[i] != arreglo[j])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        arreglo[i] = generar(min, max);
+                        j = -1;
+                    }
+                }
+
             }
             return arreglo;
         }
--- Laboratorio9/Laboratorio95/Program.cs
+++ Laboratorio9/Laboratorio95/Program.cs
@@ -25,10 +25,7 @@
             try
             {
                 numero = Int32.Parse(Console.ReadLine());
-                if (numero > 0)
-                    return numero;
-                else
-                    Console.WriteLine("El número debe ser mayor a cero");
+                return numero;
             }
             catch (Exception e)
             {
@@ -44,15 +41,33 @@ class Aleatorios
     private static Random random = new Random();
     public static int generar(int min, int max)
     {
-        return random.Next(min, max);
+        return random.Next(min, max + 1);
     }
 
     public static int[] arreglo(int n, int min, int max)
     {
         int[] arreglo = new int[n];
+
+        if (n > ((max + 1) - min))
+            throw new ArgumentException("El tamaño del arreglo es mayor que el rango de valores posibles, por lo que los valores no pueden ser unicos.");
+
+
         for (int i = 0; i < n; i++)
         {
             arreglo[i] = generar(min, max);
+            for (int j = 0; j < i; j++)
+            {
+               if (arreglo[i] != arreglo[j])
+                {
+                    continue;
+                }
+               else
+                {
+                    arreglo[i] = generar(min, max);
+                    j = -1;
+                }
+            }
+
         }
         return arreglo;
     }
