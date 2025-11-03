a37630766b1455722b290d8330fda228719be786
Ricardo De Freitas <rdefreitas@gmail.com>
2025-11-03 16:27:30 -0500

    Laboratorio 15 finalizado.

---
+++ Laboratorio15/Laboratorio154/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Laboratorio154.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Suma</title>
+</head>
+<body>
+    <h1>Suma de dos números </h1>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Numero 1:"></asp:Label>&nbsp;<asp:TextBox ID="txbNumero1" runat="server"></asp:TextBox>
+            <asp:Label ID="Label2" runat="server" Text="Numero 2:"></asp:Label>&nbsp;<asp:TextBox ID="txbNumero2" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSumar" runat="server" Text="Sumar" OnClick="btnSumar_Click" />
+        </div>
+        <div>
+
+            <asp:Label ID="lblResultado" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Laboratorio15/Laboratorio154/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laboratorio154
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSumar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double numero1 = Convert.ToDouble(txbNumero1.Text);
+                double numero2 = Convert.ToDouble(txbNumero2.Text);
+
+                double suma = numero1 + numero2;
+
+                lblResultado.Text = "El resultado de la suma es: " + suma.ToString();
+            }
+            catch (FormatException)
+            {
+                Page.ClientScript.RegisterClientScriptBlock(typeof(Page), "Alert", "window.alert('Por favor, ingrese números válidos.');", true);
+            }
+            catch (OverflowException)
+            {
+                Page.ClientScript.RegisterClientScriptBlock(typeof(Page), "Alert", "window.alert('Los números ingresados son demasiado grandes o pequeños.');", true);
+            }
+            catch (Exception ex)
+            {
+                Page.ClientScript.RegisterClientScriptBlock(typeof(Page), "Alert", "window.alert('Ocurrió un error inesperado.');", true);
+            }
+
+        }
+    }
+}
