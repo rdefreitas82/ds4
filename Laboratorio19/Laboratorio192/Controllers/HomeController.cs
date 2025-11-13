4e0db90e5766b2c3dd4880f82c39daaffceb841b
Ricardo De Freitas <rdefreitas@gmail.com>
2025-11-13 18:57:04 -0500

    Laboratorio 19 finalizado.

---
+++ Laboratorio19/Laboratorio192/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio192.Controllers
+{
+    public class HomeController : Controller
+    {
+        string port = "44345";
+        public ActionResult Index()
+        {
+            var url = $"https://localhost:{port}/api/Values/Get";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "apllication/jaon";
+            request.Accept = "application/json";
+
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) ViewBag.error = false;
+                        using (StreamReader objReader = new StreamReader (strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd();
+                            ViewBag.message = responseBody;
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Laboratorio19/Laboratorio193/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio193.Controllers
+{
+    public class HomeController : Controller
+    {
+        string port = "44345";
+        // GET: Home
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                id = 1;
+            }
+            var url = $"https://localhost:{port}/api/Values/Get/{id}";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "apllication/jaon";
+            request.Accept = "application/json";
+
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) ViewBag.error = false;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd();
+                            ViewBag.message = responseBody;
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+
+            }
+            return View();
+        }
+    }
+}
